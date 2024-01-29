667009ea8cf77499ae6dd91422ff0efb3729a05e
Md Ismail <mdismaeelkhan345@gmail.com>
2024-01-29 19:20:57 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const textArea = document.getElementById("textArea");
+
+function showData() {
+  let texData = localStorage.getItem("text");
+  if (texData) {
+    textArea.value = texData;
+  }
+}
+function savaData() {
+
+  localStorage.setItem("text", textArea.value);
+}
+
+showData();
+textArea.addEventListener("input", () => {
+  savaData();
+});
